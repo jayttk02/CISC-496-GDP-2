ed63c16a5c49b56fe12d9f160c3616ff57a1254c
Osamu <spytrever@gmail.com>
2024-02-05 18:28:30 -0500

    Added Health UI
    Health UI

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public GameObject player;
+    public static int healthCount = 3;
+    public GameObject[] playerLivesUI;
+    public GameObject gameOver;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {     
+        if (healthCount == 3)
+        {
+            playerLivesUI[0].SetActive(true);
+            playerLivesUI[1].SetActive(true);
+            playerLivesUI[2].SetActive(true);
+        }
+        if (healthCount == 2)
+        {
+            playerLivesUI[0].SetActive(true);
+            playerLivesUI[1].SetActive(true);
+            playerLivesUI[2].SetActive(false);
+        }
+        if (healthCount == 1)
+        {
+            playerLivesUI[0].SetActive(true);
+            playerLivesUI[1].SetActive(false);
+            playerLivesUI[2].SetActive(false);
+        }
+        if (healthCount == 0)
+        {
+            playerLivesUI[0].SetActive(false);
+            playerLivesUI[1].SetActive(false);
+            playerLivesUI[2].SetActive(false);
+
+            gameOver.SetActive(true);           
+            player.GetComponent<PlayerMovement>().enabled = false;           
+        }
+    }
+}
