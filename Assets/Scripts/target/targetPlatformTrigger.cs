7bad74b293aa6d06883fd5e063ff642e4ccda9cd
stefandippolito <dippolitostefan@gmail.com>
2024-03-24 17:23:32 -0400

    FinishedRoughNewLevel2
    Finished adding targets and moving platforms for Newlevel2

--- Assets/Scripts/target/targetPlatformTrigger.cs
+++ Assets/Scripts/target/targetPlatformTrigger.cs
@@ -4,10 +4,7 @@ using UnityEngine;
 
 public class targetPlatformTrigger : MonoBehaviour
 {
-    
-
-    // Update is called once per frame
-    
+        
     private void OnTriggerEnter2D(Collider2D other) {
         if (other.gameObject.name == "Bullet(Clone)"){
             Destroy(other);
