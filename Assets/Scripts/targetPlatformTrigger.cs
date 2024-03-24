484ac28b6247873a19d28fe0c62b0b9bc1b2cdaf
stefandippolito <dippolitostefan@gmail.com>
2024-03-23 23:12:58 -0400

    Updated Trigger Platform script
    Updated Trigger Platform script so now if both targets are hit then platform appears

--- Assets/Scripts/targetPlatformTrigger.cs
+++ Assets/Scripts/targetPlatformTrigger.cs
@@ -4,23 +4,14 @@ using UnityEngine;
 
 public class targetPlatformTrigger : MonoBehaviour
 {
-    public GameObject obj;
-    // Start is called before the first frame update
-    void Start()
-    {
-        obj.SetActive(false);
-    }
+    
 
     // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
     private void OnTriggerEnter2D(Collider2D other) {
         if (other.gameObject.name == "Bullet(Clone)"){
             Destroy(other);
             Destroy(gameObject);
-            obj.SetActive(true);
             
         }
         
